f01791d90b4848287f997f813dbb6fc37146fe2b
JoaoScheleder <jg.scheleder@hotmail.com>
2026-03-21 09:44:34 -0300

    feat: add email sending functionality with Mail struct and templates

---
+++ cmd/web/mailer.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"sync"
+	"time"
+
+	"github.com/vanng822/go-premailer/premailer"
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  mail.Encryption
+	FromAddress string
+	FromName    string
+	Wait        *sync.WaitGroup
+	MailerChan  *chan Message
+	ErrorChan   *chan error
+	DoneChan    *chan bool
+}
+
+type Message struct {
+	From        string
+	FromName    string
+	To          string
+	Subject     string
+	Attachments []string
+	Data        any
+	DataMap     map[string]any
+	Template    string
+}
+
+func (m *Mail) SendMail(msg Message) error {
+	if msg.Template == "" {
+		msg.Template = "mail"
+	}
+
+	if msg.From == "" {
+		msg.From = m.FromAddress
+	}
+
+	if msg.FromName == "" {
+		msg.FromName = m.FromName
+	}
+
+	msg.DataMap = map[string]any{
+		"message": msg.Data,
+	}
+
+	formattedMsg, err := m.buildHtmlMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	plainMsg, err := m.buildPlainMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	server := mail.NewSMTPClient()
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.Username
+	server.Password = m.Password
+	server.Encryption = m.Encryption
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	smtpClient, err := server.Connect()
+	if err != nil {
+		return err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(msg.FromName + "<" + msg.From + ">")
+	email.AddTo(msg.To)
+	email.SetSubject(msg.Subject)
+	email.SetBody(mail.TextPlain, plainMsg)
+	email.AddAlternative(mail.TextHTML, formattedMsg)
+
+	for _, attachment := range msg.Attachments {
+		email.AddAttachment(attachment)
+	}
+
+	if err := email.Send(smtpClient); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Mail) buildHtmlMessage(msg Message) (string, error) {
+	templateToRender := PATH_TO_TEMPLATES + "/" + msg.Template + ".html.gohtml"
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	formattedMessage := tpl.String()
+	formattedMessage, err = m.inlineCSS(formattedMessage)
+	if err != nil {
+		return "", err
+	}
+
+	return formattedMessage, nil
+}
+
+func (m *Mail) inlineCSS(formattedMsg string) (string, error) {
+	options := premailer.Options{
+		RemoveClasses:     true,
+		CssToAttributes:   true,
+		KeepBangImportant: true,
+	}
+
+	prem, err := premailer.NewPremailerFromString(formattedMsg, &options)
+
+	if err != nil {
+		return "", err
+	}
+
+	html, err := prem.Transform()
+	if err != nil {
+		return "", err
+	}
+
+	return html, nil
+}
+
+func (m *Mail) buildPlainMessage(msg Message) (string, error) {
+	templateToRender := PATH_TO_TEMPLATES + "/" + msg.Template + ".plain.gohtml"
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	plainMessage := tpl.String()
+
+	return plainMessage, nil
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	mail "github.com/xhit/go-simple-mail/v2"
 )
 
 func (app *Config) routes() http.Handler {
@@ -24,6 +25,36 @@ func (app *Config) routes() http.Handler {
 	mux.Post("/register", app.PostRegisterPage)
 
 	mux.Get("/activate", app.ActivateAccount)
+
+	mux.Get("/test-email", func(w http.ResponseWriter, r *http.Request) {
+
+		m := Mail{
+			Domain:      "localhost",
+			Host:        "localhost",
+			Port:        1025,
+			Username:    "",
+			Password:    "",
+			Encryption:  mail.EncryptionNone,
+			FromAddress: "no-reply@example.com",
+			FromName:    "Example App",
+		}
+
+		msg := Message{
+			To:       "test@example.com",
+			Subject:  "Test Email",
+			Template: "mail",
+			Data:     "MailHog test email from subscription_service",
+		}
+
+		if err := m.SendMail(msg); err != nil {
+			app.ErrorLog.Printf("send test email: %v", err)
+			http.Error(w, "unable to send test email: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("test email sent to MailHog; open http://localhost:8025"))
+	})
 
 	return mux
 }
